f163d34acba46bf967e48e56479ae035e5594024
kolesnikov <kolesnikov@greensight.ru>
2019-08-20 17:25:57 +0300

    #48166 checkboxes

--- html/src/views/UiKit/UiKitCheckboxes.vue
+++ html/src/views/UiKit/UiKitCheckboxes.vue
@@ -1,8 +1,56 @@
 <template>
-    <section class="section ui-kit-section"></section>
+    <section class="section ui-kit-section">
+        <h2 class="ui-kit-section__hl">
+            Радиокнопки
+        </h2>
+        <p class="text-grey">
+            Мастер радиобаттоны
+        </p>
+        <form>
+            <p>
+                <v-check v-model="checkbox_checked1" name="checkbox-1" value="1">По умолчанию</v-check>
+                <v-check v-model="checkbox_checked2" name="checkbox-2" value="2" disabled>Disabled</v-check>
+                <v-check v-model="checkbox_checked3" name="checkbox-3" value="3">
+                    Мастер радиобаттоны
+                </v-check>
+                <v-check
+                    v-model="checkbox_checked4"
+                    name="checkbox-1"
+                    value="Радиобаттон с длинным текстом включен и не задизейблен"
+                >
+                    Радиобаттон с длинным текстом включен и не задизейблен
+                </v-check>
+            </p>
+            <p>Выбрано По умолчанию: {{ checkbox_checked1 }}</p>
+            <p>Выбрано disabled: {{ checkbox_checked2 }}</p>
+            <p>Выбрано Мастер радиобаттоны: {{ checkbox_checked3 }}</p>
+            <p>Выбрано Радиобаттон с длинным текстом включен и не задизейблен: {{ checkbox_checked4 }}</p>
+
+            <p>
+                <v-check v-model="checkboxes_checked" name="checkboxes" value="1">1</v-check>
+                <v-check v-model="checkboxes_checked" name="checkboxes" value="2">2</v-check>
+                <v-check v-model="checkboxes_checked" name="checkboxes" value="3">3</v-check>
+            </p>
+            <p>Выбрано: {{ checkboxes_checked }}</p>
+        </form>
+    </section>
 </template>
 <script>
+import VCheck from '../../components/controls/VCheck/VCheck.vue';
+
 export default {
-    name: 'ui-kit-checkboxes',
+    name: 'ui-kit-checkbox-buttons',
+    components: {
+        VCheck,
+    },
+    data() {
+        return {
+            checkbox_checked1: false,
+            checkbox_checked2: false,
+            checkbox_checked3: false,
+            checkbox_checked4: false,
+            checkboxes_checked: [],
+        };
+    },
 };
 </script>
