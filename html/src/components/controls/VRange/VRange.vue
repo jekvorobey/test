ccf569d0b30ee75c7662674ad0a76b0b6e1c663d
kolesnikov <kolesnikov@greensight.ru>
2019-08-22 17:32:43 +0300

    #48171 range slider

--- html/src/components/controls/VRange/VRange.vue
+++ html/src/components/controls/VRange/VRange.vue
@@ -1,22 +1,30 @@
 <template>
     <div class="v-range">
         <div ref="body" class="v-range__body" />
-        <div>{{ value_internal }}</div>
-        <div>{{ min }}</div>
-        <div>{{ max }}</div>
+        <div class="v-range__container">
+            <span :key="index" v-for="(value, index) in value_internal">
+                <label class="v-range__label">
+                    <slot name="label" :index="index">{{ isSingle ? '' : index === 0 ? 'от' : 'до' }}</slot>
+                </label>
+                <input class="v-range__input" type="number" :value="value" @change="onInput($event, index)" />
+            </span>
+        </div>
     </div>
 </template>
 
 <script>
 import noUiSlider from 'nouislider';
 import './VRange.css';
+import { setTimeout } from 'timers';
 
 export default {
     name: 'v-range',
     props: {
         value: {
-            type: Number,
-            default: 0,
+            type: Array,
+            default() {
+                return [0];
+            },
         },
         min: {
             type: Number,
@@ -26,36 +34,95 @@ export default {
             type: Number,
             default: 100,
         },
+        step: {
+            type: Number,
+            default: 1,
+        },
+        format: {
+            type: Object,
+            default() {
+                return undefined;
+            },
+        },
     },
     data() {
+        const isSingle = this.value.length < 2;
         return {
+            isSingle,
             value_internal: this.value,
+            options: {
+                start: this.value,
+                connect: isSingle ? 'lower' : true,
+                step: this.step,
+                range: {
+                    min: this.min,
+                    max: this.max,
+                },
+                format: this.format,
+            },
         };
     },
+    watch: {
+        value_internal(value) {
+            this.$emit('input', value);
+        },
+
+        value() {
+            for (let i = 0; i < this.value.length; i++) {
+                if (this.value_internal[i] !== this.value[i]) {
+                    this.value_internal = this.value;
+                    this.slider.set(this.value_internal);
+                    return;
+                }
+            }
+        },
+
+        min(value) {
+            this.options.range.min = value;
+        },
+
+        max(value) {
+            this.options.range.max = value;
+        },
+
+        step(value) {
+            this.options.step = value;
+        },
+
+        format(value) {
+            this.options.format = value;
+        },
+
+        options: {
+            handler(value) {
+                this.slider.updateOptions(this.value, true);
+            },
+            deep: true,
+        },
+    },
     methods: {
+        onInput(e, index) {
+            this.value_internal[index] = Number(e.target.value);
+            this.slider.set(this.value_internal);
+        },
         onChange(values, handle, unencoded, tap, positions) {
-            this.value_internal = values[0];
+            this.value_internal = values;
         },
     },
     mounted() {
         const { body } = this.$refs;
-        this.slider = noUiSlider.create(body, {
-            start: this.value,
-            connect: true,
-            range: {
-                min: this.min,
-                max: this.max,
-            },
-        });
+        this.slider = noUiSlider.create(body, this.options);
         this.slider.on('update', this.onChange);
         this.slider.on('change', this.onChange);
     },
 
-    beforeDestroy() {
-        this.slider.off('update');
-        this.slider.off('change');
-        this.slider.destroy();
-        this.slider = null;
+    destroyed() {
+        setTimeout(() => {
+            this.slider.off('update');
+            this.slider.off('change');
+            this.slider.destroy();
+            this.slider = null;
+        }, 250);
     },
 };
 </script>
--- html/src/views/UiKit/UiKitRangeSlider.vue
+++ html/src/views/UiKit/UiKitRangeSlider.vue
@@ -1,6 +1,31 @@
 <template>
     <section class="section ui-kit-section">
-        <v-range :value="30"></v-range>
+        <h2 class="ui-kit-section__hl">
+            Слайдер диапазона
+        </h2>
+        <a href="https://refreshless.com/nouislider/" target="_blank" rel="noopener noreferrer" title="nouislider">
+            https://refreshless.com/nouislider/
+        </a>
+        <p class="text-grey">
+            Мастер инпут type="number"
+        </p>
+        <p>
+            <input type="number" :value="max" @change="e => (max = +e.target.value)" />
+        </p>
+        <p class="text-grey">
+            Слайдеры
+        </p>
+        <v-range style="width:50%;" :min="min" :max="max" :format="format" />
+        <br />
+        <br />
+        <v-range v-model="value" style="width:50%;" :min="min" :max="max" :step="step">
+            <template v-slot:label>
+                бабло
+            </template>
+        </v-range>
+        <br />
+        <br />
+        <v-range v-model="values" style="width:50%;" :min="min" :max="max" :step="step" :format="format" />
     </section>
 </template>
 <script>
@@ -10,6 +35,26 @@ export default {
     name: 'ui-kit-range-sliders',
     components: {
         VRange,
+    },
+    data() {
+        return {
+            value: [100],
+            values: [100, 500],
+            step: 10,
+            min: 0,
+            max: 1000,
+            format: {
+                // 'to' the formatted value. Receives a number.
+                to(value) {
+                    return Math.round(value);
+                },
+                // 'from' the formatted value.
+                // Receives a string, should return a number.
+                from(value) {
+                    return Number(value);
+                },
+            },
+        };
     },
 };
 </script>
