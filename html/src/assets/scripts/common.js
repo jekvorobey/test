8b0f1722aaa22afa499bc5e5fd9c49a571306b8c
kolesnikov <kolesnikov@greensight.ru>
2020-02-07 17:54:31 +0300

    fix linq-es6 prototype injection

--- html/src/assets/scripts/common.js
+++ html/src/assets/scripts/common.js
@@ -1,3 +1,4 @@
+import 'core-js/stable/symbol';
 import 'core-js/stable/object';
 import 'core-js/stable/promise';
 import 'core-js/stable/array';
@@ -10,6 +11,7 @@ import 'reflect-metadata';
 
 import './polyfills/insertAdjacentElement';
 import './polyfills/classlist-polyfill';
+import './polyfills/linq-es6';
 import 'custom-event-polyfill';
 import 'intersection-observer';
 import 'focus-visible';
@@ -20,8 +22,8 @@ import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import 'lazysizes/plugins/respimg/ls.respimg';
 import 'lazysizes/plugins/blur-up/ls.blur-up';
 
-lazysizes.cfg.blurupMode = 'auto';
-
 import './constants';
 import './enums';
 import './regex';
+
+lazysizes.cfg.blurupMode = 'auto';
