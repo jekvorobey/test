d2c5c053298ccd6773147d10344e2eea77378cf1
kolesnikov <kolesnikov@greensight.ru>
2020-03-03 16:49:03 +0300

    #53174 CatalogBannerListCard refactoring

--- html/src/components/CatalogProductList/CatalogBannerListCard/CatalogBannerListCard.vue
+++ html/src/components/CatalogProductList/CatalogBannerListCard/CatalogBannerListCard.vue
@@ -1,29 +1,32 @@
 <template>
     <li class="catalog-banner-list-card">
         <div class="catalog-banner-list-card__img" v-once>
-            <v-picture v-if="item.image && item.image.id">
-                <template v-if="isObject">
-                    <source :data-srcset="img" type="image/webp" media="(min-width: 1024px)" />
-                    <img class="blur-up lazyload v-picture__img" :data-src="defaultImg" alt="" />
-                </template>
-                <template v-else>
-                    <img class="blur-up lazyload v-picture__img" :data-src="item.image" alt="" />
-                </template>
+            <v-picture>
+                <source
+                    v-if="item.desktopImage && item.desktopImage.id"
+                    :data-srcset="desktopImg"
+                    type="image/webp"
+                    media="(min-width: 1024px)"
+                />
+                <source
+                    v-if="item.tabletImage && item.tabletImage.id"
+                    :data-srcset="tabletImg"
+                    type="image/webp"
+                    media="(min-width: 480px)"
+                />
+                <source
+                    v-if="item.mobileImage && item.mobileImage.id"
+                    :data-srcset="mobileImg"
+                    type="image/webp"
+                    media="(max-width: 479px)"
+                />
+                <img class="blur-up lazyload v-picture__img" :data-src="defaultImg" alt="" />
             </v-picture>
         </div>
 
-        <div class="catalog-banner-list-card__panel" v-once>
-            <div v-if="item.upperText" class="catalog-banner-list-card__panel-upper-text">
-                {{ item.upperText }}
-            </div>
-            <div class="catalog-banner-list-card__panel-title">
-                {{ item.title }}
-            </div>
-            <div v-if="item.bottomText" class="catalog-banner-list-card__panel-bottom-text">
-                {{ item.bottomText }}
-            </div>
-            <v-button v-if="item.btnText" class="btn--outline catalog-banner-list-card__panel-btn">
-                {{ item.btnText }}
+        <div v-if="item.button" class="catalog-banner-list-card__panel" :class="panelClasses" v-once>
+            <v-button class="catalog-banner-list-card__panel-btn" :class="btnClasses" :to="item.button.url || '/'">
+                {{ item.button.text }}
             </v-button>
         </div>
     </li>
@@ -35,6 +38,27 @@ import VPicture from '../../controls/VPicture/VPicture.vue';
 
 import { generatePictureSourcePath } from '../../../util/images';
 import './CatalogBannerListCard.css';
+
+const btnType = Object.freeze({
+    WHITE: 'white',
+    BLACK: 'black',
+    OUTLINE_BLACK: 'outline_black',
+    OUTLINE_WHITE: 'outline_white',
+});
+
+const btnLocation = Object.freeze({
+    TOP: 'top',
+    BOTTOM: 'bottom',
+
+    LEFT: 'left',
+    RIGHT: 'right',
+
+    TOP_LEFT: 'top_left',
+    TOP_RIGHT: 'top_right',
+
+    BOTTOM_LEFT: 'bottom_left',
+    BOTTOM_RIGHT: 'bottom_right',
+});
 
 export default {
     name: 'catalog-banner-list-card',
@@ -54,16 +78,74 @@ export default {
     },
 
     computed: {
-        img() {
-            return generatePictureSourcePath(300, 300, this.item.image.id, 'webp');
+        mobileImg() {
+            return generatePictureSourcePath(320, 320, this.item.mobileImage.id, 'webp');
+        },
+
+        tabletImg() {
+            return generatePictureSourcePath(540, 360, this.item.tabletImage.id, 'webp');
+        },
+
+        desktopImg() {
+            return generatePictureSourcePath(600, 400, this.item.desktopImage.id, 'webp');
         },
 
         defaultImg() {
-            return generatePictureSourcePath(300, 300, this.item.image.id, this.item.image.sourceExt);
+            return generatePictureSourcePath(600, 400, this.item.desktopImage.id, this.item.desktopImage.sourceExt);
         },
 
-        isObject() {
-            return this.item.image instanceof Object;
+        btnClasses() {
+            const classes = [];
+            switch (this.item.button.type) {
+                case btnType.WHITE:
+                    classes.push('btn--white');
+                    break;
+                case btnType.OUTLINE_WHITE:
+                    classes.push('btn--white');
+                    classes.push('btn--outline');
+                    break;
+                case btnType.OUTLINE_BLACK:
+                    classes.push('btn--outline');
+                    break;
+                default:
+                    break;
+            }
+            return classes;
+        },
+
+        panelClasses() {
+            const classes = [];
+            switch (this.item.button.location) {
+                case btnLocation.LEFT:
+                    classes.push('catalog-banner-list-card__panel--left');
+                    break;
+                case btnLocation.RIGHT:
+                    classes.push('catalog-banner-list-card__panel--right');
+                    break;
+
+                case btnLocation.TOP:
+                    classes.push('catalog-banner-list-card__panel--top');
+                    break;
+                case btnLocation.TOP_LEFT:
+                    classes.push('catalog-banner-list-card__panel--top-left');
+                    break;
+                case btnLocation.TOP_RIGHT:
+                    classes.push('catalog-banner-list-card__panel--top-right');
+                    break;
+
+                case btnLocation.BOTTOM:
+                    classes.push('catalog-banner-list-card__panel--bottom');
+                    break;
+                case btnLocation.BOTTOM_LEFT:
+                    classes.push('catalog-banner-list-card__panel--bottom-left');
+                    break;
+                case btnLocation.BOTTOM_RIGHT:
+                    classes.push('catalog-banner-list-card__panel--bottom-right');
+                    break;
+                default:
+                    break;
+            }
+            return classes;
         },
     },
 };
