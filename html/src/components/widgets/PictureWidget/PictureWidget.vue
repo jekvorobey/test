3205076677da9e5dcc5bf1e065ea432bac429a36
vasiliev <vasiliev@greensight.ru>
2020-04-07 13:13:43 +0300

    #47463 simple widgets has been added

---
+++ html/src/components/widgets/PictureWidget/PictureWidget.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="picture-widget">
+        <v-picture class="picture-widget__picture" v-if="content" :image="content" />
+    </div>
+</template>
+
+<script>
+import VPicture from '@controls/VPicture/VPicture.vue';
+
+import './PictureWidget.css';
+
+export default {
+    name: 'picture-widget',
+    components: {
+        VPicture,
+    },
+
+    props: {
+        content: {
+            type: String,
+            default: '',
+        }
+    }
+}
+</script>
