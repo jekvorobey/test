bad8d9f9e28d144518199bb2243a07d37a56ec37
kolesnikov <kolesnikov@greensight.ru>
2020-04-13 11:46:00 +0300

    minor fix order modules

--- html/src/store/modules/Profile/modules/Orders/index.js
+++ html/src/store/modules/Profile/modules/Orders/index.js
@@ -1,9 +1,10 @@
+import { DEFAULT_PAGE } from '@constants';
+import { sortDirections } from '@enums';
+import { sortFields } from '@enums/order';
+
 import actions from './actions';
 import mutations from './mutations';
 import getters from './getters';
-import { sortDirections } from '@enums';
-import { sortFields } from '@enums/order';
-import { DEFAULT_PAGE } from '@constants';
 
 export const NAME = 'orders';
 export const LOAD_PATH = 'loadPath';
@@ -17,6 +18,7 @@ export const DELIVERIES = 'deliveries';
 export const ORDER_DIRECTION = 'orderDirection';
 export const ORDER_FIELD = 'orderField';
 export const ACTIVE_PAGE = 'activePage';
+export const RANGE = 'range';
 
 const PAGE_SIZE = 'pageSize';
 
@@ -29,6 +31,7 @@ export default function createModule() {
             [LOAD_PATH]: '',
 
             [ORDERS]: [],
+            [RANGE]: 0,
             [ORDER_DETAILS]: { [ORDER]: {}, [DELIVERIES]: [] },
 
             [ORDER_DIRECTION]: sortDirections.DESC,
