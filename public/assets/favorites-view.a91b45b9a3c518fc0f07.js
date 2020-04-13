7c7e06be5e717761421d136a585e8e295051607b
vasiliev <vasiliev@greensight.ru>
2020-04-13 16:27:03 +0300

    sync assets

--- public/assets/favorites-view.a91b45b9a3c518fc0f07.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[59],{"0Bec":function(n,t,e){},fPNc:function(n,t,e){"use strict";e.r(t);e("0Bec");var s={name:"favorites"},c=e("KHd+"),i=Object(c.a)(s,(function(){var n=this.$createElement;return(this._self._c||n)("div",{staticClass:"favorites"},[this._v("\n    Страница избранного\n")])}),[],!1,null,null,null);t.default=i.exports}}]);
