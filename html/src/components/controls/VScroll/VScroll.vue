9872d9da1dc6f9df00d2e21b5ad0105f4e5840bd
Ilya Vasiliev <ilushka911@inbox.ru>
2020-04-30 15:35:59 +0300

    #58987 custom scroll has been added

---
+++ html/src/components/controls/VScroll/VScroll.vue
@@ -0,0 +1,18 @@
+<template>
+    <vuescroll class="v-scroll">
+        <slot />
+    </vuescroll>
+</template>
+
+<script>
+import Vuescroll from 'vuescroll';
+
+import './VScroll.css';
+
+export default {
+    name: 'v-scroll',
+    components: {
+        Vuescroll,
+    },
+};
+</script>
