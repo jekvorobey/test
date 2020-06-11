e8e87e8cee64a537704f6a3d88c2ee6d8696bb47
vasiliev <vasiliev@greensight.ru>
2020-06-11 17:09:30 +0300

    #59918 Courier return form added

--- html/src/components/profile/ReturnForm/ReturnForm.vue
+++ html/src/components/profile/ReturnForm/ReturnForm.vue
@@ -1,11 +1,101 @@
 <template>
-    <div>
-        Форма возврата
+    <div class="return-form">
+        <div class="return-form__header">
+            <h3 class="return-form__header-title">
+                Выберите способ возврата
+            </h3>
+            <ul class="return-form__list">
+                <checkout-option-card
+                    class="return-form__list-item"
+                    v-for="method in receiveMethods"
+                    :key="method.id"
+                    :selected="method.id === selectedReceiveMethodID"
+                    readonly
+                    @cardClick="onSetReturnMethod(method.id)"
+                >
+                    <p class="text-bold">{{ method.title }}</p>
+                    <p class="text-grey text-sm">{{ method.description }}</p>
+                </checkout-option-card>
+            </ul>
+        </div>
+        <component :is="selectedReturnMethod" />
+        <div class="return-form__controls">
+            <v-button class="return-form__submit">
+                Оформить возврат
+            </v-button>
+            <v-button class="return-form__back-btn btn--transparent">
+                Назад
+            </v-button>
+        </div>
     </div>
 </template>
 
 <script>
+import VButton from '@controls/VButton/VButton.vue';
+
+import CheckoutOptionCard from '@components/checkout/CheckoutOptionCard/CheckoutOptionCard.vue';
+import ReturnCourierForm from './ReturnCourierForm.vue';
+import ReturnOutpostForm from './ReturnOutpostForm.vue';
+import ReturnPostForm from './ReturnPostForm.vue';
+
+import { returnMethods } from '@enums/profile.js';
+
+import './ReturnForm.css';
+
+const receiveMethods = [
+    {
+        id: 1,
+        title: 'Курьеру',
+        description: 'Общий вес товаров более 5 кг',
+    },
+    {
+        id: 2,
+        title: 'В пункт выдачи заказов',
+    },
+    {
+        id: 3,
+        title: 'В постамат',
+    },
+];
+
 export default {
     name: 'return-form',
+
+    components: {
+        VButton,
+
+        CheckoutOptionCard,
+    },
+
+    data() {
+        return {
+            selectedReceiveMethodID: 1,
+        };
+    },
+
+    computed: {
+        selectedReturnMethod() {
+            switch (this.selectedReceiveMethodID) {
+                case returnMethods.COURIER:
+                    return ReturnCourierForm;
+                case returnMethods.OUTPOST:
+                    return ReturnOutpostForm;
+                case returnMethods.POST:
+                    return ReturnPostForm;
+                default:
+                    return null;
+            }
+        },
+    },
+
+    methods: {
+        onSetReturnMethod(methodId) {
+            this.selectedReceiveMethodID = methodId;
+        },
+    },
+
+    beforeMount() {
+        this.receiveMethods = receiveMethods;
+    },
 };
 </script>
--- html/src/store/modules/Profile/modules/ReturnPage/index.js
+++ html/src/store/modules/Profile/modules/ReturnPage/index.js
@@ -1,17 +1,19 @@
-import actions from './actions';
-import mutations from './mutations';
-import getters from './getters';
+import actions from './actions'
+import mutations from './mutations'
+import getters from './getters'
 
-import { returnFormSteps } from '@enums/profile';
+import { returnFormSteps } from '@enums/profile'
 
-export const NAME = 'returnPage';
+export const NAME = 'returnPage'
 
-export const PRODUCTS = 'products';
-export const SELECTED_PRODUCTS = 'selectedProducts';
+export const PRODUCTS = 'products'
+export const SELECTED_PRODUCTS = 'selectedProducts'
+export const COURIER_DELIVERY = 'courierDelivery'
+export const ADDRESSES = 'addresses';
 
-export const STEP = 'step';
+export const STEP = 'step'
 
-export default function createModule() {
+export default function createModule () {
     return {
         name: NAME,
         namespaced: true,
@@ -70,10 +72,125 @@ export default function createModule() {
                 },
             ],
             [SELECTED_PRODUCTS]: [],
+            [COURIER_DELIVERY]: [
+                {
+                    id: 1,
+                    title: 'Отправление №124589524-1',
+                    time: '24 июня (пн), с 10:00 до 18:00',
+                    products: [
+                        {
+                            id: 531,
+                            categoryCodes: ['dlya_makiyazha'],
+                            code: 'shampun-dlya-uplotneniya-volos-plumping-wash',
+                            image: {
+                                id: 3,
+                                sourceExt: 'jpg',
+                            },
+                            name: 'Второй шамнупь для уплотнение Plumping.Wash с очень длинным названием',
+                            price: {
+                                currency: 'RUB',
+                                value: 300,
+                            },
+                            oldPrice: {
+                                currency: 'RUB',
+                                value: 450,
+                            },
+                            productId: 2,
+                            stock: {
+                                qty: 1,
+                                storeId: 1,
+                            },
+                            type: 'product',
+                            quantity: 1,
+                            reason: 'Вскрыта оригинальная упаковка',
+                        },
+                    ],
+                    selectedAddress: null,
+                },
+                {
+                    id: 2,
+                    title: 'Отправление №124589524-2',
+                    time: '28 июня (пт), с 10:00 до 18:00',
+                    products: [
+                        {
+                            id: 532,
+                            categoryCodes: ['dlya_makiyazha'],
+                            code: 'shampun-dlya-uplotneniya-volos-plumping-wash',
+                            description: 'Цвет 27',
+                            image: {
+                                id: 3,
+                                sourceExt: 'jpg',
+                            },
+                            name: 'Шампунь для уплотнения волос Plumping.Wash',
+                            price: {
+                                currency: 'RUB',
+                                value: 3000,
+                            },
+                            oldPrice: {
+                                currency: 'RUB',
+                                value: 4200,
+                            },
+                            productId: 2,
+                            stock: {
+                                qty: 3,
+                                storeId: 1,
+                            },
+                            type: 'product',
+                            quantity: 1,
+                            reason: 'Не тот товар в заказе',
+                        },
+                    ],
+                    selectedAddress: null,
+                },
+            ],
+            [ADDRESSES]: [
+                {
+                    area: 'поселение Воскресенское',
+                    city: 'г Москва',
+                    flat: '21',
+                    block: '',
+                    floor: '3',
+                    house: 'д 4',
+                    porch: 'undefined',
+                    region: 'г Москва',
+                    street: '',
+                    comment: '2',
+                    geo_lat: '55.5295556',
+                    geo_lon: '37.4456939',
+                    intercom: '',
+                    area_guid: 'f3860e12-eee7-4a1a-8ece-0d82b2ce497f',
+                    city_guid: '0c5b2444-70a0-4932-980c-b4dc0d3f02b5',
+                    post_index: '108803',
+                    region_guid: '0c5b2444-70a0-4932-980c-b4dc0d3f02b5',
+                    country_code: 'RU',
+                    id: 44,
+                },
+                {
+                    area: 'ул. Парковая',
+                    city: 'г Москва',
+                    flat: '31',
+                    block: '',
+                    floor: '3',
+                    house: 'д 4',
+                    porch: 'undefined',
+                    region: 'г Москва',
+                    street: '',
+                    comment: '2',
+                    geo_lat: '55.5295556',
+                    geo_lon: '37.4456939',
+                    intercom: '',
+                    area_guid: 'f421421412ce497f',
+                    city_guid: '0c5b2444-70a0-4932-9841d3f02b5',
+                    post_index: '108803',
+                    region_guid: '0c5b2444-70a0-4932-9841d3f02b5',
+                    country_code: 'RU',
+                    id: 45,
+                },
+            ],
         },
 
         actions,
         mutations,
         getters,
-    };
+    }
 }
