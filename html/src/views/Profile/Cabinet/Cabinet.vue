95306bbbc36870a21cf4f431567e8ebad7b5b09f
vasiliev <vasiliev@greensight.ru>
2020-06-17 18:18:25 +0300

    #61340 SaveBtn added to Cabinet

--- html/src/views/Profile/Cabinet/Cabinet.vue
+++ html/src/views/Profile/Cabinet/Cabinet.vue
@@ -99,6 +99,10 @@
 
         <cabinet-requisites-panel v-if="referralPartner" class="cabinet-view__panel" />
 
+        <div class="cabinet-view__controls">
+            <v-button class="cabinet-view__save-btn" @click="onSave">Сохранить</v-button>
+        </div>
+
         <transition name="fade">
             <password-edit-modal v-if="$isServer || (hasPhone && isPasswordEditOpen)" />
         </transition>
@@ -313,6 +317,17 @@ export default {
         onOpenPasswordModal() {
             this[CHANGE_MODAL_STATE]({ name: modalName.profile.PASSWORD_EDIT, open: true });
         },
+
+        onSave() {
+            this[CHANGE_MODAL_STATE]({
+                    name: modalName.general.NOTIFICATION,
+                    open: true,
+                    state: {
+                        title: 'Уведомление',
+                        message: 'Все данные сохранены',
+                    },
+                });
+        }
     },
 
     async serverPrefetch() {
