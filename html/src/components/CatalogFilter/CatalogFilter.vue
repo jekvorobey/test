a338a21832a64a18b672bd56a768805933d759e9
Alexey Soloviev <kibnerk@gmail.com>
2020-06-19 18:10:18 +0300

    #60157 sticky block & fixes for filters

--- html/src/components/CatalogFilter/CatalogFilter.vue
+++ html/src/components/CatalogFilter/CatalogFilter.vue
@@ -5,7 +5,7 @@
             key-field="id"
             :items="accordionFilters"
             :item-expanded="(item) => item.isExpanded"
-            :item-toggled="(item) => (item.isExpanded = !!!item.isExpanded)"
+            :item-toggled="(item) => onIsExpandedClick(item.id)"
         >
             <template v-slot:content="{ item: filter }">
                 <div class="catalog-filter__filters-range" v-if="filter.item.type === 'range'">
@@ -18,7 +18,11 @@
                         @input="onRangeChange($event, filter.item.name)"
                     />
                 </div>
-                <div class="catalog-filter__filters-check" v-else-if="filter.item.type === 'check'">
+                <div
+                    class="catalog-filter__filters-check"
+                    v-else-if="filter.item.type === 'check'"
+                    :ref="'check' + filter.item.id"
+                >
                     <v-check
                         v-for="option in (filter.showMore ? filter.item.items.filter((i, key) => key < maxCountFilters) : filter.item.items)"
                         :id="`${filter.item.name}-${option.id}`"
@@ -32,9 +36,9 @@
                     <button
                         class="catalog-filter__filters-more"
                         @click="() => onShowMoreClick(filter.item.id)"
-                        v-if="filter.showMore && filter.item.items.length >= maxCountFilters"
+                        v-if="filter.moreMax"
                     >
-                        Показать все
+                        {{ filter.showMore ? 'Показать все' : 'Свернуть' }}
                     </button>
                 </div>
                 <div class="catalog-filter__filters-check" v-else-if="filter.item.type === 'radio'">
@@ -90,6 +94,7 @@ export default {
             },
             maxCountFilters: 8,
             showMore: [],
+            isExpanded: [],
         };
     },
 
@@ -97,9 +102,9 @@ export default {
         ...mapGetters(CATALOG_MODULE, [FILTER_SEGMENTS, ROUTE_SEGMENTS]),
         ...mapState(CATALOG_MODULE, [FILTERS]),
         ...mapState('route', {
-            type: state => state.params.type,
-            code: state => state.params.code,
-            entityCode: state => state.params.entityCode,
+            type: (state) => state.params.type,
+            code: (state) => state.params.code,
+            entityCode: (state) => state.params.entityCode,
         }),
 
         accordionFilters() {
@@ -109,32 +114,51 @@ export default {
                           id: f.id,
                           item: f,
                           title: f.title,
-                          isExpanded: true,
+                          isExpanded: this.isExpanded.find(({ id }) => id === f.id).state,
                           showMore: this.showMore.find(({ id }) => id === f.id).state,
+                          moreMax: f.items ? f.items.length >= this.maxCountFilters : false,
                       };
                   })
                 : [];
         },
     },
     created() {
-        this.showMore = [
-            ...this.filters.map(({ type, items, id }) => {
-                return {
-                    id,
-                    state: type === 'check' && items.length >= this.maxCountFilters ? true : false,
-                };
-            }),
-        ];
+        this.initFiltersOptions();
     },
     methods: {
         onRadioChange(e, value) {
             const { type, entityCode, code, routeSegments } = this;
 
             if (!routeSegments.includes(value)) routeSegments.push(value);
-            routeSegments = routeSegments.filter(s => s === value);
+            routeSegments = routeSegments.filter((s) => s === value);
 
             const path = concatCatalogRoutePath(type, entityCode, code, routeSegments);
             this.$router.replace({ path });
+        },
+
+        initFiltersOptions() {
+            this.showMore = [
+                ...this.filters.map(({ type, items, id }) => {
+                    return {
+                        id,
+                        state: type === 'check' && items.length >= this.maxCountFilters ? true : false,
+                    };
+                }),
+            ];
+            this.isExpanded = [
+                ...this.filters.map(({ id }) => {
+                    return {
+                        id,
+                        state: true,
+                    };
+                }),
+            ];
+            this.isExpanded.forEach(({ id }) => {
+                if (this.$refs['check' + id]) {
+                    const el = this.$refs['check' + id].parentNode;
+                    el.removeAttribute('style');
+                }
+            });
         },
 
         onCheckChange(e, value) {
@@ -156,12 +180,25 @@ export default {
         },
 
         onShowMoreClick(id) {
-            const moreIndex = this.showMore.findIndex(el => el.id === id);
-            const moreItem = this.showMore.find(el => el.id === id);
+            const moreIndex = this.showMore.findIndex((el) => el.id === id);
+            const moreItem = this.showMore.find((el) => el.id === id);
             this.showMore.splice(moreIndex, 1, {
                 ...moreItem,
-                state: false,
+                state: !moreItem.state,
             });
+            this.$emit('updateSticky');
+            const el = this.$refs['check' + id].parentNode;
+            el.removeAttribute('style');
+        },
+
+        onIsExpandedClick(id) {
+            const moreIndex = this.isExpanded.findIndex((el) => el.id === id);
+            const moreItem = this.isExpanded.find((el) => el.id === id);
+            this.isExpanded.splice(moreIndex, 1, {
+                ...moreItem,
+                state: !moreItem.state,
+            });
+            this.$emit('updateSticky');
         },
 
         onRangeChange(e, name) {
@@ -191,6 +228,20 @@ export default {
             const path = concatCatalogRoutePath(type, entityCode, code, routeSegments);
             this.$router.replace({ path });
         }, 500);
+    },
+    watch: {
+        filters() {
+            this.initFiltersOptions();
+        },
+
+        sortValue(value, oldValue) {
+            if (value !== oldValue) {
+                this.$router.replace({
+                    path: this.$route.path,
+                    query: { orderField: value.field, orderDirection: value.direction },
+                });
+            }
+        },
     },
 };
 </script>
--- html/src/views/Catalog/Catalog.vue
+++ html/src/views/Catalog/Catalog.vue
@@ -2,22 +2,19 @@
     <section class="section catalog-view">
         <div class="container catalog-view__header">
             <breadcrumbs class="catalog-view__breadcrumbs">
-                <breadcrumb-item key="main" to="/">
-                    Главная
-                </breadcrumb-item>
-                <breadcrumb-item :key="type" :to="breadcrumbRootUrl">
-                    {{ $t(`productGroups.title.${type}`) }}
-                </breadcrumb-item>
-                <breadcrumb-item v-if="entityCode" :key="entityCode" :to="generateBreadcrumbUrl(null)">
-                    {{ productGroup && productGroup.name }}
-                </breadcrumb-item>
+                <breadcrumb-item key="main" to="/">Главная</breadcrumb-item>
+                <breadcrumb-item :key="type" :to="breadcrumbRootUrl">{{
+                    $t(`productGroups.title.${type}`)
+                }}</breadcrumb-item>
+                <breadcrumb-item v-if="entityCode" :key="entityCode" :to="generateBreadcrumbUrl(null)">{{
+                    productGroup && productGroup.name
+                }}</breadcrumb-item>
                 <breadcrumb-item
                     v-for="category in breadcrumbs"
                     :key="category.id"
                     :to="generateBreadcrumbUrl(category.code)"
+                    >{{ category.name }}</breadcrumb-item
                 >
-                    {{ category.name }}
-                </breadcrumb-item>
             </breadcrumbs>
 
             <catalog-banner-card class="catalog-view__banner" v-if="productGroup.banner" :item="productGroup.banner">
@@ -33,29 +30,36 @@
                     <source :data-srcset="mobileImg.webp" type="image/webp" media="(min-width: 320px)" />
                     <source :data-srcset="mobileImg.orig" media="(min-width: 320px)" />
                 </template>
-                <img v-if="defaultImg" class="blur-up lazyload v-picture__img" :data-src="defaultImg" alt="" />
+                <img v-if="defaultImg" class="blur-up lazyload v-picture__img" :data-src="defaultImg" alt />
             </catalog-banner-card>
         </div>
         <section class="section">
-            <div class="container catalog-view__grid">
-                <div class="catalog-view__side-panel" v-if="!isTabletLg && categories.length > 0">
-                    <ul class="catalog-view__side-panel-categories">
-                        <category-tree-item
-                            class="catalog-view__side-panel-categories-item"
-                            v-for="category in categories"
-                            :item="category"
-                            :key="category.id"
-                        />
-                    </ul>
-                    <catalog-filter class="catalog-view__side-panel-filters" />
-                    <v-button
-                        class="btn--outline catalog-view__side-panel-clear-btn"
-                        :class="!isFiltersPage && 'is-disabled'"
-                        :to="clearFilterUrl"
-                        replace
-                    >
-                        Очистить фильтры
-                    </v-button>
+            <div class="container catalog-view__grid" data-v-sticky-container>
+                <div
+                    class="catalog-view__side-panel"
+                    v-if="!isTabletLg && categories.length > 0"
+                    v-sticky="stickyOptions"
+                    ref="sticky"
+                >
+                    <div data-v-sticky-inner>
+                        <ul class="catalog-view__side-panel-categories">
+                            <category-tree-item
+                                class="catalog-view__side-panel-categories-item"
+                                v-for="category in categories"
+                                :item="category"
+                                :key="category.id"
+                            />
+                        </ul>
+
+                        <catalog-filter class="catalog-view__side-panel-filters" @updateSticky="updateSticky" />
+                        <v-button
+                            class="btn--outline catalog-view__side-panel-clear-btn"
+                            :class="!isFiltersPage && 'is-disabled'"
+                            :to="clearFilterUrl"
+                            replace
+                            >Очистить фильтры</v-button
+                        >
+                    </div>
                 </div>
                 <div class="catalog-view__main">
                     <div class="catalog-view__main-header">
@@ -108,9 +112,8 @@
                             btn-class="btn--outline catalog-view__main-controls-btn"
                             @click="onShowMore"
                             :show-preloader="showMore"
+                            >Показать ещё</show-more-button
                         >
-                            Показать ещё
-                        </show-more-button>
                         <v-pagination :value="activePage" :page-count="pagesCount" @input="onPageChanged" />
                     </div>
                 </div>
@@ -155,12 +158,11 @@
                                 class="btn--outline catalog-view__modal-filter-clear-btn"
                                 :to="clearFilterUrl"
                                 replace
+                                >Очистить</v-button
                             >
-                                Очистить
-                            </v-button>
-                            <v-button class="catalog-view__modal-filter-close-btn" @click="filterModal = !filterModal">
-                                Показать
-                            </v-button>
+                            <v-button class="catalog-view__modal-filter-close-btn" @click="filterModal = !filterModal"
+                                >Показать</v-button
+                            >
                         </div>
                     </v-sticky>
                 </template>
@@ -177,10 +179,7 @@
                     этом. Мы заботимся о надёжных поставщиках, качестве товаров и безопасной оплате. А что делать вам?
                     Просто наслаждаться покупками. Для экономии не нужен повод, поэтому мы каждый день даём вам скидки
                     на популярные товары самых разных категорий.
-
-                    <template v-slot:btn="{ isExpanded }">
-                        {{ isExpanded ? 'Скрыть' : 'Показать больше' }}
-                    </template>
+                    <template v-slot:btn="{ isExpanded }">{{ isExpanded ? 'Скрыть' : 'Показать больше' }}</template>
                 </v-expander>
             </div>
         </section>
@@ -249,6 +248,7 @@ import { sortFields } from '@enums/catalog';
 import { sortDirections, fileExtension, httpCodes } from '@enums';
 import { MIN_SCROLL_VALUE } from '@constants';
 
+import '@plugins/sticky';
 import '@images/sprites/cross-small.svg';
 import './Catalog.css';
 
@@ -289,6 +289,12 @@ export default {
             sortOptions,
             filterModal: false,
             showMore: false,
+            stickyOptions: {
+                topSpacing: 60,
+                bottomSpacing: 0,
+                containerSelector: '[data-v-sticky-container]',
+                innerWrapperSelector: '[data-v-sticky-inner]',
+            },
         };
     },
 
@@ -381,6 +387,7 @@ export default {
         code(value) {
             const category = this[ACTIVE_CATEGORY];
             if (category) $retailRocket.addCategoryView(category.id);
+            this.updateSticky();
         },
 
         sortValue(value, oldValue) {
@@ -400,6 +407,13 @@ export default {
 
         scrollTo(options) {
             if (!this.$isServer) window.scrollTo(options);
+        },
+
+        updateSticky() {
+            this.$refs.sticky._stickySidebar.updateSticky();
+            setTimeout(() => {
+                this.$refs.sticky._stickySidebar.updateSticky();
+            }, 500);
         },
 
         generateBreadcrumbUrl(categoryCode) {
