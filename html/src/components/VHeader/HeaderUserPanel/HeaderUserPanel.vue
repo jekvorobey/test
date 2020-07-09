58e59dcc6666eba6cd6cf9b8515e9ccea0d45912
vasiliev <vasiliev@greensight.ru>
2020-07-09 14:41:07 +0300

    #62222 HeaderUserPanel visual edits

--- html/src/components/VHeader/HeaderUserPanel/HeaderUserPanel.vue
+++ html/src/components/VHeader/HeaderUserPanel/HeaderUserPanel.vue
@@ -20,7 +20,7 @@
                 <cart-header-panel class="header-user-panel__item-cart">
                     <price class="text-medium header-user-panel__item-sum" v-bind="productItemsSum" />
                     &nbsp;&nbsp;
-                    <button>
+                    <button class="header-user-panel__cart-btn">
                         <v-svg name="cart-middle" width="24" height="24" />
                         <span class="text-bold header-user-panel__item-count">{{ cartItemsCount }}</span>
                     </button>
