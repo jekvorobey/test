6163733fd3d8a5382a46b7ed3747d7d2e2c29da0
Карпачев Егор <e.karpachev@qsoft.ru>
2021-08-05 12:50:11 +0600

    removed: unused

--- html/src/views/VerificationCode/VerificationCode.vue
+++ html/src/views/VerificationCode/VerificationCode.vue
@@ -15,27 +15,14 @@
                 <v-button class="not-found-view__main-btn" to="/">Активировать</v-button>
             </div>
         </div>
-
-        <section class="section">
-            <div class="container">
-                <retail-rocket-container data-retailrocket-markup-block="5efda11c97a52833a0d006e7" v-bind:data-auth="hasSession" />
-            </div>
-        </section>
     </section>
 </template>
 
 <script>
 import VButton from '@controls/VButton/VButton.vue';
-import VSlider from '@controls/VSlider/VSlider.vue';
-
-import CatalogProductCard from '@components/CatalogProductCard/CatalogProductCard.vue';
-
-import { mapState, mapActions, mapGetters } from 'vuex';
 
 import { NAME as MODAL_MODULE, MODALS } from '@store/modules/Modal';
 import { CHANGE_MODAL_STATE } from '@store/modules/Modal/actions';
-
-import { NAME as FEATURED_MODULE, FEATURED_PRODUCTS } from '@store/modules/Featured';
 import { FETCH_FEATURED_PRODUCTS } from '@store/modules/Featured/actions';
 
 import { NAME as FAVORITES_MODULE } from '@store/modules/Favorites';
@@ -44,9 +31,6 @@ import { TOGGLE_FAVORITES_ITEM } from '@store/modules/Favorites/actions';
 import metaMixin from '@plugins/meta';
 import { breakpoints, modalName } from '@enums';
 import './VerificationCode.css';
-import { NAME as AUTH_MODULE, HAS_SESSION } from '@store/modules/Auth';
-import RetailRocketContainer from '@components/RetailRocketContainer/RetailRocketContainer.vue';
-
 
 export default {
     name: 'verification-code',
@@ -69,10 +53,6 @@ export default {
     },
 
     methods: {
-        ...mapActions(MODAL_MODULE, [CHANGE_MODAL_STATE]),
-        ...mapActions(FEATURED_MODULE, [FETCH_FEATURED_PRODUCTS]),
-        ...mapActions(FAVORITES_MODULE, [TOGGLE_FAVORITES_ITEM]),
-
 
         onPreview(code) {
             this[CHANGE_MODAL_STATE]({
