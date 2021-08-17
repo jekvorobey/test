de5774b1cc372320eb705bf7623a035df5ac7ce6
Максим Купров <maxim.kyprov@ylab.io>
2021-08-17 12:20:01 +0300

    IBT-460: fix redirect when error

--- html/src/router/middleware/activateCustomer.js
+++ html/src/router/middleware/activateCustomer.js
@@ -1,5 +1,5 @@
-import { httpCodes, injectionType } from '@enums';
-import { breakMiddleware } from '@util/router';
+import { injectionType } from '@enums';
+import { createNotFoundRoute } from '@util/router';
 
 import { NAME as AUTH_MODULE } from '@store/modules/Auth';
 import { ACTIVATE_CUSTOMER } from '@store/modules/Auth/actions';
@@ -16,10 +16,9 @@ export default async function activateCustomer({ to, next, container }) {
         if (appContext.isServer) {
             await dispatch(`${AUTH_MODULE}/${ACTIVATE_CUSTOMER}`, { customerId, signature });
 
-            return next({ name: 'Cabinet' });
+            next({ name: 'Cabinet' });
         } else next(false);
     } catch (error) {
-        if (error.status === httpCodes.FORBIDDEN) breakMiddleware(appContext, next, null, httpCodes.FORBIDDEN);
-        else next({ name: 'Account' });
+        next(createNotFoundRoute(to));
     }
 }
