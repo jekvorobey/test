bb48fda138fc2527857d3d60952f938f342101c0
Evgeniy Mokrov <e.mokrov@ylab.io>
2021-11-26 18:42:59 +0300

    IBT-437: fix masterclass view

--- html/src/views/Masterclass/Masterclass.vue
+++ html/src/views/Masterclass/Masterclass.vue
@@ -137,7 +137,10 @@
                             </v-link> -->
                         </div>
 
-                        <div v-if="places" class="container container--tablet master-class-view__panel-right-section">
+                        <div
+                            v-if="places && places.length > 0"
+                            class="container container--tablet master-class-view__panel-right-section"
+                        >
                             <p class="text-bold master-class-view__panel-right-hl">Место проведения</p>
 
                             <ol :class="{ list: places.length > 1 }">
@@ -456,7 +459,11 @@
             </div>
         </section>
 
-        <section v-if="places" ref="map" class="section master-class-view__section master-class-view__map">
+        <section
+            v-if="places && places.length > 0"
+            ref="map"
+            class="section master-class-view__section master-class-view__map"
+        >
             <div class="container master-class-view__map-container">
                 <h2 class="container container--tablet master-class-view__section-hl">Место проведения</h2>
 
@@ -481,7 +488,7 @@
                                 <template v-if="places.length > 1">{{ index + 1 }}.</template> {{ place.name }},
                                 {{ place.address }}
                             </div>
-                            <div>{{ place.description }}</div>
+                            <div v-if="place.description">{{ place.description }}</div>
                         </div>
 
                         <ul class="master-class-view__map-gallery">
@@ -922,7 +929,7 @@ export default {
 
         mapCoords() {
             const { places } = this;
-            return places.length > 1 ? [0, 0] : (places[0] && places[0].coords) || null;
+            return places && places.length > 1 ? [0, 0] : (places[0] && places[0].coords) || null;
         },
 
         bannerImage() {
@@ -992,15 +999,11 @@ export default {
             const { stages = [] } = this[MASTERCLASS] || {};
 
             return stages.map((s) => {
-                const dateFrom = getDate(s.date_from);
-                const dateTo = getDate(s.date_to);
+                const dateFrom = `${getDate(s.date_from).toLocaleDateString(this[LOCALE], dayMonthLongDateSettings)}`;
+                const dateTo = `${getDate(s.date_to).toLocaleDateString(this[LOCALE], dayMonthLongDateSettings)}`;
                 const timeFrom = getDate(`${s.date_from} ${s.timeFrom}`);
                 const timeTo = getDate(`${s.date_to} ${s.timeTo}`);
-                const dateString =
-                    dateFrom !== dateTo
-                        ? `${dateFrom.toLocaleDateString(this[LOCALE], dayMonthLongDateSettings)}
-                        - ${dateTo.toLocaleDateString(this[LOCALE], dayMonthLongDateSettings)}`
-                        : `${dateFrom.toLocaleDateString(this[LOCALE], dayMonthLongDateSettings)}`;
+                const dateString = dateFrom !== dateTo ? dateFrom + `-` + dateTo : dateFrom;
 
                 return `${dateString},
                 ${timeFrom.toLocaleTimeString(this[LOCALE], hourMinuteTimeSettings)}
