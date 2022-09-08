42c978b7b79b1818ee4dab08dd0e7f08767ad50a
Alexander <aleksandr-shiryaev-1993@mail.ru>
2022-09-08 17:15:45 +0300

    BX-7823: add block alert cookies

---
+++ html/src/components/CookieAlert/CookieAlert.vue
@@ -0,0 +1,55 @@
+<template>
+    <transition name="slip">
+        <div v-if="show" class="cookie-alert" :class="{ 'fixed-cookie-alert': isTabletLg }">
+            <div class="cookie-alert__wrapper">
+                <div class="cookie-alert__text">
+                    {{ textToShow }}
+                    <router-link to="/agreements/privacy-policy">Политикой конфиденциальности</router-link>
+                </div>
+                <button @click="onClose" class="cookie-alert_btn">
+                    <v-svg name="trash" width="24" height="23" modifier="cookie-alert-close"/>
+                </button>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+    import './CookieAlert.css';
+    import '@images/sprites/trash.svg';
+
+    import VSvg from "@controls/VSvg/VSvg.vue";
+    import {$cookie} from "@services";
+
+    export default {
+        name: 'cookie-alert',
+        components: {VSvg},
+        data() {
+            return {
+                show: false,
+            };
+        },
+        methods: {
+            onClose() {
+                this.show = false;
+
+                $cookie.set('agreeCookie', true, {
+                    maxAge: 5000,
+                    path: '/',
+                });
+            },
+        },
+        computed: {
+            isTabletLg() {
+                return this.$mq.tabletLg;
+            },
+            textToShow() {
+                return this.isTabletLg ? 'Мы собираем куки. Посещая страницы сайта, вы соглашаетесь с ' : 'На сайте используются файлы cookies, которые его делают более удобным для каждого пользователя. Посещая страницы сайта, вы соглашаетесь с '
+            }
+        },
+        created() {
+            this.show = !$cookie.get('agreeCookie');
+        }
+    };
+</script>
+
