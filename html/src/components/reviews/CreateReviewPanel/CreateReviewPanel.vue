10dcffe47b707b5cf4b715e32c025adf97ec47c4
Leonid <l.borisov@ibt.ru>
2023-03-15 13:12:03 +0300

    11901: refactoring

--- html/src/components/reviews/CreateReviewPanel/CreateReviewPanel.vue
+++ html/src/components/reviews/CreateReviewPanel/CreateReviewPanel.vue
@@ -1,13 +1,14 @@
 <template>
-    <div class="create-review-panel"ref="ratingSection">
+    <div class="create-review-panel" ref="ratingSection">
         <h2 class="create-review-panel__title">Отзывы</h2>
         <form class="create-review-panel__form" enctype="multipart/form-data" @submit.prevent="onSubmit">
-            <input type="hidden" :value="code" name="object_code" />
-            <input type="hidden" :value="type" name="object_type" />
+            <input type="hidden" :value="code" name="object_code"/>
+            <input type="hidden" :value="type" name="object_type"/>
 
             <div class="create-review-panel__form-review">
                 <div class="create-review-panel__form-rating">
-                    <span class="create-review-panel__form-rating-text"
+                    <span
+                        class="create-review-panel__form-rating-text"
                         :class="{ 'create-review-panel__form-rating-text--error': ratingError }"
                     >
                         Ваша оценка
@@ -20,7 +21,7 @@
                         name="rating"
                     >
                         <template v-slot:activeLabel>
-                            <v-svg name="star-small" width="20" height="20" />
+                            <v-svg name="star-small" width="20" height="20"/>
                         </template>
 
                         <template v-slot:inactiveLabel>
@@ -60,20 +61,20 @@
                     </v-input>
                 </div>
 
-<!--                <div class="create-review-panel__form-cell">-->
-<!--                    <h3 class="create-review-panel__form-title">Фото и видео товара</h3>-->
+                <!--                <div class="create-review-panel__form-cell">-->
+                <!--                    <h3 class="create-review-panel__form-title">Фото и видео товара</h3>-->
 
-<!--                    <span class="create-review-panel__form-subtitle">Не более 10 файлов, jpeg, png, heic, mp4, mov</span>-->
+                <!--                    <span class="create-review-panel__form-subtitle">Не более 10 файлов, jpeg, png, heic, mp4, mov</span>-->
 
-<!--                    <v-file-->
-<!--                        class="create-review-panel__form-files"-->
-<!--                        @change="onFilesChanged"-->
-<!--                        :accepted-types="fileAcceptedTypes"-->
-<!--                        :max-file-size="5242880"-->
-<!--                    >-->
-<!--                        <span class="create-review-panel__form-files-desc">Выберите фото</span>-->
-<!--                    </v-file>-->
-<!--                </div>-->
+                <!--                    <v-file-->
+                <!--                        class="create-review-panel__form-files"-->
+                <!--                        @change="onFilesChanged"-->
+                <!--                        :accepted-types="fileAcceptedTypes"-->
+                <!--                        :max-file-size="5242880"-->
+                <!--                    >-->
+                <!--                        <span class="create-review-panel__form-files-desc">Выберите фото</span>-->
+                <!--                    </v-file>-->
+                <!--                </div>-->
 
                 <v-button class="create-review-panel__form-submit" type="submit" :disabled="isDisabled">
                     Отправить отзыв
@@ -89,8 +90,8 @@ import VButton from '@controls/VButton/VButton.vue';
 import VFile from '@controls/VFile/VFile.vue';
 import VRating from '@controls/VRating/VRating.vue';
 
-import { mimeType } from '@enums';
-import validationMixin, { required } from '@plugins/validation';
+import {mimeType} from '@enums';
+import validationMixin, {required} from '@plugins/validation';
 import '@images/sprites/star-empty-small-error.svg';
 import '@images/sprites/star-empty-small.svg';
 import '@images/sprites/star-small.svg';
@@ -141,7 +142,13 @@ export default {
 
     computed: {
         fileAcceptedTypes() {
-            return [mimeType.image.JPEG, mimeType.image.PNG, mimeType.image.HEIC, mimeType.video.MP4, mimeType.video.MOV];
+            return [
+                mimeType.image.JPEG,
+                mimeType.image.PNG,
+                mimeType.image.HEIC,
+                mimeType.video.MP4,
+                mimeType.video.MOV,
+            ];
         },
 
         bodyError() {
@@ -164,9 +171,9 @@ export default {
                 this.$refs.ratingSection.scrollIntoView({
                     behavior: 'smooth',
                     block: 'center',
-                    inline: 'center'
+                    inline: 'center',
                 });
-                return
+                return;
             }
 
             const formData = new FormData(e.target);
