b027f91c3b64f3539788bd068f2dd4dea6656fd1
Alexander Shiryaev <aleksandr-shiryaev-1993@mail.ru>
2023-04-05 12:36:54 +0300

    BX-9279: fix diplomas & use getters

--- html/src/store/modules/Auth/getters.js
+++ html/src/store/modules/Auth/getters.js
@@ -1,1 +1,7 @@
-export default {};
+import {USER} from "@store/modules/Auth/index";
+
+export const IS_USER_PROFESSIONAL = 'IS_USER_PROFESSIONAL';
+
+export default {
+    [IS_USER_PROFESSIONAL]: (state) => (state[USER] && state[USER].canBuy) || false,
+};
